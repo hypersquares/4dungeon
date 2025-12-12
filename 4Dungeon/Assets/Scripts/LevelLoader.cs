83e7b98318280b6c41256928ad55c671240b253b
Sterling Chargois <sterlingchargois@gmail.com>
2025-12-12 02:09:22 -0500

    Stuff

--- 4Dungeon/Assets/Scripts/LevelLoader.cs
+++ 4Dungeon/Assets/Scripts/LevelLoader.cs
@@ -6,22 +6,25 @@ public class LevelLoader : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         print("LevelExit");
-        print(other.tag);
-        if (other.tag == "LevelExit")
+        if (other.tag == "NextLevelExit")
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (other.tag == "PrevLevelExit")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 }
